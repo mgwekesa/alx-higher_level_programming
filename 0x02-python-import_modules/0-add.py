55ff73642978b3e2dfecbade7a2a7aba94bc2ffc
mgwekesa <mgwekesa@gmail.com>
2022-06-05 20:19:08 +0300

    updating the importing of a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("<a {:d}> + <b {:d}> = <add({:d}, {:d}) {:d}>\
+    print("<a {}> + <b {}> = <add({}, {}) {}>\
     ".format(a, b, a, b, add(a, b)))
