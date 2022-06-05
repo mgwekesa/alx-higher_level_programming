a0a6c429d4153f267bfe8db3f2e42a9d774bbcd1
mgwekesa <mgwekesa@gmail.com>
2022-06-05 22:15:37 +0300

    modifying the output

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        quit(1)
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
     op = ["+", "-", "*", "/"]
@@ -15,4 +15,4 @@ if __name__ == "__main__":
             break
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            quit(1)
+            exit(1)
