e60b6c6002d6046f27281613af663f3fd4844fcf
mgwekesa <mgwekesa@gmail.com>
2022-06-05 20:14:55 +0300

    imports a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
-
-print("<a {:d}> + <b {:d}> = <add({:d}, {:d}) {:d}>\
-".format(a, b, a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("<a {:d}> + <b {:d}> = <add({:d}, {:d}) {:d}>\
+    ".format(a, b, a, b, add(a, b)))
