9802a261f226785ac64ae0b4bb54a68aa1e45aae
mgwekesa <mgwekesa@gmail.com>
2022-06-06 12:09:28 +0300

    needed to use 'and' instead of 'or'

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    ln = len(my_list)
-    if (idx >= 0 or idx < ln):
+    if (idx >= 0 and idx < len(my_list)):
         return (my_list[idx])
     else:
         return None
