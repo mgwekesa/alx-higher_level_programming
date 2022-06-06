9beb27af540b69ecdfb014ceb537bff5aef94509
mgwekesa <mgwekesa@gmail.com>
2022-06-06 12:24:33 +0300

    ensuring that the list contains elements before printing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        for elm in my_list[::-1]:
-            print("{:d}".format(my_list[-elm]))
+        for idx in my_list:
+            print("{:d}".format(my_list[-idx]))
