f78c87deea9aa2a52957f35ad819a35c09f7a31e
mgwekesa <mgwekesa@gmail.com>
2022-06-09 19:32:40 +0300

    used bitwise operation instead

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    comm_set = []
-    for elm in set_1:
-        if elm in set_2:
-            comm_set.append(elm)
-    return (comm_set)
+    return (set_1 & set_2)
