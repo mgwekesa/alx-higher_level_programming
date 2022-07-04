ef2163a71d121330741d7dfc774b5df245b61936
mgwekesa <mgwekesa@gmail.com>
2022-07-04 23:45:55 +0300

    returns a list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" returns the list of available attributes and methods of an object """
+
+
+def lookup(obj):
+    """
+    0-lookup.py
+    takes the object as an attribute
+
+    Returns:
+        list object
+    """
+    return (obj)
