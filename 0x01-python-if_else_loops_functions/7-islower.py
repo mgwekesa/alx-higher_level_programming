d6d1a7e1a158142d2c0053d9a8443b87033c375e
mgwekesa <mgwekesa@gmail.com>
2022-07-09 13:34:27 +0300

    checks for lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,5 +4,6 @@ def islower(c):
     for letter in range(97, 123):
         if (c == chr(letter)):
             return (True)
-    else:
-        return (False)
+    for letter in range(65, 91):
+        if (c == (letter)):
+            return (False)
