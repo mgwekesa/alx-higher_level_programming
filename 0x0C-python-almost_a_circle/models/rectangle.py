2d5e3ec321fefe16bc3b8498847819bc3e43376b
mgwekesa <mgwekesa@gmail.com>
2022-07-11 19:52:53 +0300

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,22 +8,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """ initializing attributes """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    """ getter and setter for height """
-    @property
-    def height(self):
-        """ getter for height """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ setter for height """
-        self.validate("height", value)
-        self.__height = value
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     """ getter and setter for width """
     @property
@@ -34,8 +22,30 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ setter for width """
-        self.validate("width", value)
-        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        else:
+            if value <= 0:
+                raise ValueError("width must be > 0")
+            else:
+                self.__width = value
+
+    """ getter and setter for height """
+    @property
+    def height(self):
+        """ getter for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter for height """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        else:
+            if value <= 0:
+                raise ValueError("height must be > 0")
+            else:
+                self.__height = value
 
     """ getter and setter for x """
     @property
@@ -46,8 +56,13 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ setter for x """
-        self.validate("x", value)
-        self.__x = value
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("x must be >= 0")
+            else:
+                self.__x = value
 
     """ getter and setter for y """
     @property
@@ -58,14 +73,10 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """setter for y """
-        self.validate("y", value)
-        self.__y = value
-
-    def validate(self, name, value):
-        """ validates the given inputs """
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if name in ["width"] and value <= 0:
-            raise ValueError("{:s} must be > 0".format(name))
-        if name in ["x", "y"] and value < 0:
-            raise ValueError("{:s} must be >= 0".format(name))
+            raise TypeError("y must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("y must be >= 0")
+            else:
+                self.__y = value
