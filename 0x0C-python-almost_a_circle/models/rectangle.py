58184ad92b057946ffb2918512d3df9daf55d5a0
mgwekesa <mgwekesa@gmail.com>
2022-07-11 23:49:23 +0300

    Display#1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,8 +76,11 @@ class Rectangle(Base):
 
     def display(self):
         """ prints in stdout the Rectangle instance with character # """
+        for n in range(self.y):
+            print()
         for index in range(self.height):
-            print("".join(["#" for j in range(self.width)]))
+            print("".join(" " for j in range(self.x)), end="")
+            print("".join(["#" for k in range(self.width)]))
 
     def __str__(self):
         """ overides the string method """
