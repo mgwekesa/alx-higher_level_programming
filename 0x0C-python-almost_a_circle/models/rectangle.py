0dca8c14fe7914ac35e3d184692f951a8c86098f
mgwekesa <mgwekesa@gmail.com>
2022-07-12 12:13:19 +0300

    modified validation of attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,8 +22,13 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ setter for width """
-        self.validate("width", value)
-        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        else:
+            if value <= 0:
+                raise ValueError("width must be > 0")
+            else:
+                self.__width = value
 
     """ getter and setter for height """
     @property
@@ -34,8 +39,13 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ setter for height """
-        self.validate("height", value)
-        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        else:
+            if (value <= 0):
+                raise ValueError("height must be > 0")
+            else:
+                self.__height = value
 
     """ getter and setter for x """
     @property
@@ -46,8 +56,13 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ setter for x """
-        self.validate("x", value)
-        self.__x = value
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("x must be >= 0")
+            else:
+                self.__x = value
 
     """ getter and setter for y """
     @property
@@ -58,17 +73,13 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """setter for y """
-        self.validate("y", value)
-        self.__y = value
-
-    def validate(self, name, value):
-        """ validates the given inputs """
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if name in ["width", "height"] and value <= 0:
-            raise ValueError("{:s} must be > 0".format(name))
-        if name in ["x", "y"] and value < 0:
-            raise ValueError("{:s} must be >= 0".format(name))
+            raise TypeError("y must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("y must be >= 0")
+            else:
+                self.__y = value
 
     def area(self):
         """ returns the area value of the Rectangle instance """
