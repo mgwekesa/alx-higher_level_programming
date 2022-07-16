0ec10643f17af32c579bf345dd27f151534e35c6
mgwekesa <mgwekesa@gmail.com>
2022-07-16 22:41:13 +0300

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 
 def write_file(filename="", text=""):
     """ returns the number of characters written """
-    with open(filename, mode='w') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
