212077b6623ad3cc5e84c48831e635a5d3010f6a
mgwekesa <mgwekesa@gmail.com>
2022-07-18 22:38:14 +0300

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" class Student """
+
+
+class Student:
+    """ defines a student by first_name, last_name, age """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance """
+        return self.__dict__
